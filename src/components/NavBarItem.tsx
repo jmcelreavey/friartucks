85ffdbbc69a1e8f07b8a66c98d645bf46807bd46
Johnny <jmcelreavey@gcdtech.com>
2022-07-20 17:24:21 +0100

    💄 Shading on buttons

--- src/components/NavBarItem.tsx
+++ src/components/NavBarItem.tsx
@@ -7,12 +7,12 @@ type NavBarItemProps = {
 };
 export const NavBarItem = (props: NavBarItemProps) => {
   const { title, path, selected } = props;
-  const className = selected ? "bg-green-700 text-white" : "text-green-700";
+  const className = selected ? "bg-green-700 text-white shadow-xl" : "text-green-700";
   return (
     <li>
       <Link href={path} passHref>
         <a
-          className={`${className} uppercase font-semibold hover:text-white hover:bg-green-600 duration-600 motion-safe:hover:scale-110`}
+          className={`${className} hover:shadow-xl uppercase font-semibold hover:text-white hover:bg-green-600 duration-600 motion-safe:hover:scale-110`}
         >
           {title}
         </a>
