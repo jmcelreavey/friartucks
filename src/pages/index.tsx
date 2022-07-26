ddb7fe9056f29b9590e3d0cf05246b7059b6622e
Johnny <jmcelreavey@gcdtech.com>
2022-07-26 13:56:31 +0100

    🚨 Lint warnings - svg

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -67,7 +67,7 @@ const Home: NextPage<HomeProps> = ({ ip }: HomeProps) => {
                           {branch.isHiring && (
                             <div className="alert alert-info shadow-lg">
                               <div>
-                              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" class="stroke-current flex-shrink-0 w-6 h-6"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"></path></svg>
+                              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" className="stroke-current flex-shrink-0 w-6 h-6"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"></path></svg>
                                 <span className="ml-2 font-semibold">
                                   We&apos;re Hiring - Contact us for details.
                                 </span>
